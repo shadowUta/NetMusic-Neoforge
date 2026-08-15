899f65da7889c43c227ce2d2f50ce34c1864b605
shadowUta <182259078+shadowUta@users.noreply.github.com>
2026-08-15 22:51:51 +0800

    修复 26.2 兼容：移除已删除的 com.mojang.logging.annotations 注解

--- src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/package-info.java
+++ src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/package-info.java
@@ -1,7 +1,4 @@
-
-@MethodsReturnNonnullByDefault @ParametersAreNonnullByDefault
+@ParametersAreNonnullByDefault
 package com.github.tartaricacid.netmusic.compat.sbackpack;
-
-import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/com/github/tartaricacid/netmusic/inventory/io/package-info.java
+++ src/main/java/com/github/tartaricacid/netmusic/inventory/io/package-info.java
@@ -1,7 +1,4 @@
 @ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
 package com.github.tartaricacid.netmusic.inventory.io;
-
-import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
